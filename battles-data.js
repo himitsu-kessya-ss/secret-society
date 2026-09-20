9276347e9c3aa9f078bb721e72998e5b6eb18d32
himitsu-kessya-ss <himitsukessya-ss@mailfence.com>
2026-09-20 17:45:07 +0900

    Create battles-data.js

---
+++ battles-data.js
@@ -0,0 +1,24 @@
+// battles-data.js
+const battleEvents = [
+    {
+        eventName: "勢力内戦（2026年9月）",
+        battles: [
+            {
+                roundName: "勢力内戦 260920 第1戦",
+                url: "勢力内戦260920_1.html",
+                description: "団体総力戦（Aチーム vs Bチーム） / 参加者18名"
+            },
+            // 今後追加される同じイベントの別試合はここに追記できます
+        ]
+    },
+    {
+        eventName: "過去の大会・その他のイベント",
+        battles: [
+            {
+                roundName: "サンプル大会（例：春の陣）",
+                url: "#",
+                description: "大会のサマリや詳細へのリンク"
+            }
+        ]
+    }
+];
